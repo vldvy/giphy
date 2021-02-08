3f75fdd2a019c196ae4ff6cc782060307a4662ae
vldvy <ald.jo.a@gmail.com>
2021-02-07 23:23:21 -0300

    Se eliminan y crean aarchivos nuevos

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react'
 import './App.css';
 
+const apiURL = 'https://api.giphy.com/v1/gifs/search?api_key=U1w8OhvodRNaBYI1MtGpRmEcb1EpCAeY&q=panda&limit=25&offset=0&rating=g&lang=en';
+
 function App() {
+
+  const [gifs, setGifs] = useState([]);
+
+  useEffect(function () {
+    fetch(apiURL)
+      .then(res => res.json())
+      .then(response => {
+        const {data} = response;
+        const gifs = data.map(image => image.images.downsized_medium.url);
+        setGifs(gifs);
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="App-content">
+        <img src={gifs} alt="iamgenes"/>
+      </section>
     </div>
   );
 }
